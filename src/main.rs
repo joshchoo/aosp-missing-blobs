5046ffd25a77856f07e36c50ad7f3a78f0dc00de
Josh Choo <dev@joshuous.com>
2021-06-06 01:17:47 +0800

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::env;
 
 use aosp_missing_blobs::run;
+use clap::App;
 
 fn main() {
+    App::new("aosp-missing-blobs")
+        .version("0.3.0")
+        .author("Josh Choo <dev@joshuous.com>")
+        .about("An AOSP tool to generate a list of required missing blobs.")
+        .get_matches();
+
     let args: Vec<String> = env::args().collect();
     let paths = &args[1..];
 
